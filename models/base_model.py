86a7f0bc8de0695339bd1ede004eb0a132c03cc4
Thegermanblob <thegermanblob@gmail.com>
2021-07-03 19:20:26 +0000

    Make pep8 compliant

--- models/base_model.py
+++ models/base_model.py
@@ -41,8 +41,10 @@ class BaseModel:
     def to_dict(self):
         """ returns dictionary """
         todict = self.__dict__.copy()
-        todict["created_at"] = self.created_at.isoformat("%Y-%m-%dT%H:%M:%S.%f")
-        todict["updated_at"] = self.updated_at.isoformat("%Y-%m-%dT%H:%M:%S.%f")
+        todict["created_at"] = self.created_at.isoformat(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        todict["updated_at"] = self.updated_at.isoformat(
+            "%Y-%m-%dT%H:%M:%S.%f")
         todict["__class__"] = self.__class__.__name__
         return todict
 
